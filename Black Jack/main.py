0c12dd9374d902f77ea76883a46de7860e606c07
Miguel Azevedo <40613610+MiguelAzevedo2108@users.noreply.github.com>
2021-04-09 15:54:41 +0000

    BJ
    Black Jack

--- Black Jack/main.py
+++ Black Jack/main.py
@@ -1,16 +1,108 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+naipes = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10,
+          'Jack': 10,
+          'Queen': 10, 'King': 10, 'Ace': 11}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Card:
+
+    def __init__(self, naipe, rank):
+        self.naipe = naipe
+        self.rank = rank
+        self.value = values[rank]
+
+    def __str__(self):
+        return self.rank + " of " + self.naipe
 
 
-# Press the green button in the gutter to run the script.
+class Deck:
+
+    def __init__(self):
+        self.deck = []  # start with an empty list
+        for naipe in naipes:
+            for rank in ranks:
+                card = Card(naipe, rank)
+                self.deck.append(card)
+
+    def __str__(self):
+        deck = ''
+        for i in self.deck:
+            deck += '\n' + i.__str__()
+        return 'This deck has:' + deck
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        return self.deck.pop()
+
+
+class Hand:
+
+    def __init__(self):
+        self.cards = []
+        self.hand_value = 0
+        self.aces = 0
+
+    def add_card(self,card):
+        self.cards.append(card)
+        self.hand_value += values[card.rank]
+
+        if card.ranks == 'Ace':
+            self.aces += 1
+
+
+    def ajust_ace(self):
+
+        while self.aces > 0 and self.hand_value > 21:
+            self.hand_value -= 10
+            self.aces -= 1
+
+
+class Chips:
+
+    def __init__(self):
+        self.total = 100
+        self.bet = 0
+
+    def win_bet(self):
+        self.total += self.bet
+
+    def lose_bet(self):
+        self.total -= self.bet
+
+
+def take_bet(chips):
+    while True:
+        try:
+            chips.bet = int(input('Insert your bet: '))
+        except:
+            print('Please insert a valid amount')
+        else:
+            if chips.bet > chips.total:
+                print('Your bet cant exceed', chips.total)
+            else:
+                break
+
+
+def hit(deck, hand):
+    pass
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    deck = Deck()
+    deck.shuffle()
+    print(deck.__str__())
+    print('End')
+    hand = Hand()
+    hand.add_card(deck.deal())
+    for i in hand.cards:
+        print(i)
+    print(hand.hand_value)
